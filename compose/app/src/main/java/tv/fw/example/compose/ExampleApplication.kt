637155bf74e15163a3b10973c916d37818ea5402
josuer <josue@fireworkhq.com>
2023-02-10 12:48:36 -0600

    Compose:  `6.0.0-beta.2` Update
    Updated demo app to use `6.0.0-beta.2` sdk version

--- compose/app/src/main/java/tv/fw/example/compose/ExampleApplication.kt
+++ compose/app/src/main/java/tv/fw/example/compose/ExampleApplication.kt
@@ -2,10 +2,10 @@ package tv.fw.example.compose
 
 import android.app.Application
 import android.util.Log
+import com.firework.imageloading.glide.GlideImageLoaderFactory
+import com.firework.sdk.FireworkSdk
+import com.firework.sdk.FireworkSdkConfig
 import tv.fw.example.compose.BuildConfig.FW_CLIENT_ID
-import tv.fw.fireworksdk.FireworkSdk
-import tv.fw.fireworksdk.FireworkSdkConfig
-import tv.fw.imageloading.glide.GlideImageLoaderFactory
 
 class ExampleApplication : Application() {
     override fun onCreate() {
@@ -17,6 +17,7 @@ class ExampleApplication : Application() {
             .clientId(FW_CLIENT_ID) // Client OAUTH Id
             .userId("example app user ID") // User Id in your eco-system
             .imageLoader(GlideImageLoaderFactory.createInstance()) // glide, picasso, or your ImageLoader implementation
+            .muteOnLaunch(true)
             .build()
 
         // initialize Firework Android SDK v2
