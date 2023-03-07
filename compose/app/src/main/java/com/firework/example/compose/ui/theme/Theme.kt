aa983fb101a2ff1329f909799a6dd706c8ae2400
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2023-03-07 16:53:08 +0100

    Fix compose code format
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- compose/app/src/main/java/com/firework/example/compose/ui/theme/Theme.kt
+++ compose/app/src/main/java/com/firework/example/compose/ui/theme/Theme.kt
@@ -20,10 +20,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun FireworkComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit,
-) {
+fun FireworkComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
