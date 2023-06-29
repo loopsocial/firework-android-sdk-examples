5ba9201165fa6cee0bc2146130d26473c3280303
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2023-06-29 20:52:27 +0200

    Update AGP
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- feed_resources/app/build.gradle.kts
+++ feed_resources/app/build.gradle.kts
@@ -34,13 +34,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         allWarningsAsErrors = true
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     lint {
