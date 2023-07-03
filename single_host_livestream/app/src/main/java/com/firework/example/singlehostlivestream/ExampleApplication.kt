8d381120d4cd4afb3079502bacf771c4ff6c3847
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2023-07-03 17:58:44 +0200

    Update Single-Host Livestream to ASDK 6.3.4 (#59)
    **What**
    - Updating the Single-Host Livestream Example App to ASDK 6.3.4
    
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- single_host_livestream/app/src/main/java/com/firework/example/singlehostlivestream/ExampleApplication.kt
+++ single_host_livestream/app/src/main/java/com/firework/example/singlehostlivestream/ExampleApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.firework.example.singlehostlivestream.BuildConfig.FW_CLIENT_ID
 import com.firework.imageloading.glide.GlideImageLoaderFactory
-import com.firework.livestream.singlehost.LivestreamPlayerInitializer
+import com.firework.livestream.singlehost.SingleHostLivestreamPlayerInitializer
 import com.firework.sdk.FireworkSdk
 import com.firework.sdk.FireworkSdkConfig
 
@@ -18,9 +18,9 @@ class ExampleApplication : Application() {
             .checksumRequired(false)
             .clientId(FW_CLIENT_ID) // Client OAUTH Id
             .userId("example app user ID") // User Id in your eco-system
-            .imageLoader(GlideImageLoaderFactory.createInstance()) // glide, picasso, or your implementation
+            .imageLoader(GlideImageLoaderFactory.createInstance(context = this)) // glide, picasso, or your implementation
             // Single-Host and Multi-Host Livestreams can be used together, and single-host will be used as a fallback if multi-host is missing
-            .addLivestreamPlayerInitializer(LivestreamPlayerInitializer())
+            .addLivestreamPlayerInitializer(SingleHostLivestreamPlayerInitializer())
             .build()
 
         // initialize Firework Android SDK v6
