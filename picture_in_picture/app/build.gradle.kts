3ac503ca1bddef90691e7688dd49307f11636219
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2023-07-03 18:20:03 +0200

    Update PiP to ASDK v6.3.4 (#62)
    **What**
    - Updating the PiP app to ASDK 6.3.4
    
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- picture_in_picture/app/build.gradle.kts
+++ picture_in_picture/app/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 android {
     namespace = "com.firework.example.pictureinpicture"
 
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.firework.example.pictureinpicture"
 
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
 
         versionCode = 1
         versionName = "1.0.0"
@@ -31,13 +31,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         allWarningsAsErrors = true
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     lint {
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.6.10"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -71,7 +71,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.13.2")
 
     // Firework SDK
-    val fireworkSdkVersion = "6.2.1"
+    val fireworkSdkVersion = "6.3.4"
     implementation("com.firework:sdk:$fireworkSdkVersion")
 
     // Glide (optional image loader)
