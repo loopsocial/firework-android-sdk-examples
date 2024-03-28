5166f4a254c644a8493059e31ec1a517f89c74fa
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2024-03-28 16:13:14 +0100

    Update to ASDK v6.10.1
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- compose/app/build.gradle.kts
+++ compose/app/build.gradle.kts
@@ -94,7 +94,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    val fireworkBomVersion = "2024.03.26"
+    // Firework SDK
+    val fireworkBomVersion = "2024.03.28"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
