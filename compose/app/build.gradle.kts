a939a751a516cabb305a86927c8e135b10bb88d7
Mohsen Mirhoseini <mohsen@fireworkhq.com>
2024-04-04 20:54:31 +0200

    Fix Compose
    Signed-off-by: Mohsen Mirhoseini <mohsen@fireworkhq.com>

--- compose/app/build.gradle.kts
+++ compose/app/build.gradle.kts
@@ -83,10 +83,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material:material:1.6.4")
+    implementation("androidx.compose.material:material:1.6.5")
 
     // compose
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
