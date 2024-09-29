3be6620ef0e4398b414e5d94852d214f0e6bae7a
eggsy chen <158010156+eggsy-firework@users.noreply.github.com>
2024-09-29 14:58:28 +0800

    Update to ASDK v6.14.2 (#93)
    **What**
    
    <!-- What did change with this PR -->
    
    - Update to ASDK v6.14.2
    
    
    **Why**
    
    <!-- Why is this change is needed - Jira ticket (ASDK-xxx) or Github
    Issue (#xxx) -->
    
    - ASDK-xxx or #xxx
    
    
    **How**
    
    <!-- How did you solve the issue, like refactoring steps or newly
    created layouts -->
    
    -
    
    
    **Screenshots**
    <!-- Before and After screenshots if it is a UI change -->
    
    |  Before   |   After   |
    |-----------|-----------|
    |   image   |   image   |

--- compose/app/build.gradle.kts
+++ compose/app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Firework SDK
-    val fireworkBomVersion = "2024.09.12"
+    val fireworkBomVersion = "2024.09.27"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
