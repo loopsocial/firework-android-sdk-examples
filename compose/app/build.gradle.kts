99aec89deb50b5a6e042f47341e4c3da59107981
eggsy chen <158010156+eggsy-firework@users.noreply.github.com>
2025-01-09 19:53:59 +0800

    Upgrade native sdk version to 6.16.5 (#95)
    **What**
    
    <!-- What did change with this PR -->
    
    - Upgrade native sdk version to 6.16.5
    
    
    **Why**
    
    <!-- Why is this change is needed - Jira ticket (ASDK-xxx) or Github
    Issue (#xxx) -->
    
    - ASDK-xxx or #xxx
    
    
    **How**
    
    <!-- How did you solve the issue, like refactoring steps or newly
    created layouts -->
    
    -
    
    
    **Screenshots**
    <!-- Before and After screenshots if it is a UI change -->
    
    |  Before   |   After   |
    |-----------|-----------|
    |   image   |   image   |

--- compose/app/build.gradle.kts
+++ compose/app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Firework SDK
-    val fireworkBomVersion = "2024.12.23"
+    val fireworkBomVersion = "2025.01.09"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
