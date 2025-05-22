ddd06e833f22eea1682f52b04a8b7e40040e5bdf
Eggsy <eggsy@fireworkhq.com>
2025-05-22 20:01:19 +0800

    Upgrade to native sdk version 6.20.0

--- feed_resources/app/build.gradle.kts
+++ feed_resources/app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     // Firework SDK
-    val fireworkBomVersion = "2025.05.06"
+    val fireworkBomVersion = "2025.05.22"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
