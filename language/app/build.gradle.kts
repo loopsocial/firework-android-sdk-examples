912ded6ddba0ffe43e52606dde714a2fba4e008b
Xiaosong Lin <xiaosong@fireworkhq.com>
2025-08-04 16:06:55 +0800

    Upgrade native sdk version to 6.23.0

--- language/app/build.gradle.kts
+++ language/app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     // Firework SDK
-    val fireworkBomVersion = "2025.07.18"
+    val fireworkBomVersion = "2025.08.04"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
