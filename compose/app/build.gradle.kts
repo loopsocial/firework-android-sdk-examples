97c9ad2f3527120c0b2d8616f281db78d9ff44b3
Xiaosong Lin <xiaosong@fireworkhq.com>
2025-12-17 01:58:24 +0800

    upgrade native sdk version to 6.27.4

--- compose/app/build.gradle.kts
+++ compose/app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Firework SDK
-    val fireworkBomVersion = "2025.12.15"
+    val fireworkBomVersion = "2025.12.16"
     implementation(platform("com.firework:firework-bom:$fireworkBomVersion"))
     implementation("com.firework:sdk")
     implementation("com.firework.external.imageloading:glide") // Glide (optional image loader)
